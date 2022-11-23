50c5d1b0d61919b354e752690a30ef33c959802f
Sara Morison <rob.morison32@gmail.com>
2022-11-23 14:51:12 -0500

    Created lists for program

--- main.py
+++ main.py
@@ -8,3 +8,20 @@
 # (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections. 
 # (10 points): As a user, I want to display my completed trip in the console. 
 # (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
+
+
+destinatations = ["Boston", "New York City", "New Haven", "Hartford", "Albany"]
+
+restaurants_boston = ["Sorellina", "Toro", "Ostra", "Stockyard"]
+restuarants_new_york = ["Del Frisco's", "Per Se", "Cote", "Gramercy Tavern"]
+restuarants_new_haven = ["Zinc", "Consiglio's", "Prime 16", "Olea"]
+restaurants_hartford = ["Republic at The Linden", "The Capital Grille", "Feng Chophouse", "Sorella"]
+restaurants_albany = ["Barcelona", "Black & Blue Steak and Crab", "Grappa '72", "Athos"]
+
+modes_of_transportation = ["car", "bus", "train", "walk"]
+
+entertainment_boston = [" Go to a Red Sox game", "Go to a Celtics game", "Go to a Bruins game", "Walk the Freedom trail"]
+entertainment_new_york = ["tour Ellis Island", "Visit 9/11 memorial", "Go to a Yankees game", "Just walk around the city"]
+entertainment_new_haven = ["Visit the Yale Center for British Art", "Go to Harkness Tower", "Go to Long Wharf Park", "Visit Vietnam Memorial Park"]
+entertainment_hartford = ["See a play at the Bushnell", "Go to a baseball game", "Take a ride on the carousel", "Visit the Wadworth Atheneum Museum"]
+entertainment_albany = ["Visit the Albany Institute of History", "Visit craft breweries", "See the Schuyler Mansion", "Tour the USS Slater"]
